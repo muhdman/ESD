7be12c77bbc0393f0108efda304e4d97a0d7e92c
Aiman Ismail <73932427+muhdman@users.noreply.github.com>
2020-11-04 17:20:09 +0800

    Create Example_7.ino
    Turn ON & OFF two LEDs at different time.

---
+++ GPIO/Example_7/Example_7.ino
@@ -0,0 +1,58 @@
+#define led1 6
+#define led2 7
+
+unsigned long lastMillis1, lastMillis2;
+bool status1, status2;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(led1, OUTPUT);
+  pinMode(led2, OUTPUT);
+
+  digitalWrite(led1, HIGH);
+  digitalWrite(led2, HIGH);
+  status1 = true;
+  status2 = true;
+
+  lastMillis1 = millis();
+  lastMillis2 = millis();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+  if(status1){
+    // LED 1 will turn ON for 750 ms
+    if(millis - lastMillis1 >= 750){
+      digitalWrite(led1, LOW);
+      lastMillis1 = millis();
+      status1 = false;
+    }
+  }
+  //LED 1 will turn OFF for 350 ms
+  else{
+    if(millis - lastMillis1 >= 350){
+      digitalWrite(led1, HIGH);
+      lastMillis1 = millis();
+      status1 = true;
+    }
+  }
+
+  if(status2){
+    //LED 2 will turn ON for 400 ms
+    if(millis - lastMillis2 >= 400){
+      digitalWrite(led2, LOW);
+      lastMillis2 = millis();
+      status2 = false;
+    }
+  }
+  //LED 2 will turn OFF for 600 ms
+  else{
+    if(millis - lastMillis2 >= 600){
+      digitalWrite(led2, HIGH);
+      lastMillis2 = millis();
+      status2 = true;
+    }
+  }
+}
