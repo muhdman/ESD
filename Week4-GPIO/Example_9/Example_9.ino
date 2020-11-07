d004fe13331c12413ee70f3e60bc4d979c4f9f71
Aiman Ismail <73932427+muhdman@users.noreply.github.com>
2020-11-07 17:17:36 +0800

    Update Example_9.ino
    - Start coding

--- Week4-GPIO/Example_9/Example_9.ino
+++ Week4-GPIO/Example_9/Example_9.ino
@@ -1,9 +1,59 @@
+class ledControl {
+  bool state;
+  unsigned long lastMillis;
+  int ON, OFF, PIN;
+  
+  public:
+  ledControl(int pin, long on, long off){
+    PIN = pin;
+    pinMode(pin, OUTPUT);
+    ON = on;
+    OFF = off;
+    lastMillis = millis();
+  }
+
+  void blinking(){
+    if(state){
+    // LED 1 will turn ON for 750 ms
+      if(millis() - lastMillis >= ON){
+        digitalWrite(PIN, LOW);
+        lastMillis = millis();
+        state = false;
+      }
+    }
+    //LED 1 will turn OFF for 350 ms
+    else{
+      if(millis() - lastMillis >= OFF){
+        digitalWrite(PIN, HIGH);
+        lastMillis = millis();
+        state = true;
+      }
+    }
+  }
+};
+
+ledControl LED1(3, 781, 515);
+ledControl LED2(4, 2014, 1348);
+ledControl LED3(5, 343, 573);
+ledControl LED4(6, 678, 1839);
+ledControl LED5(7, 342, 534);
+ledControl LED6(8, 1478, 326);
+ledControl LED7(9, 1859, 351);
+ledControl LED8(10, 777, 888);
+
 void setup() {
   // put your setup code here, to run once:
-
+  Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  LED1.blinking();
+  LED2.blinking();
+  LED3.blinking();
+  LED4.blinking();
+  LED5.blinking();
+  LED6.blinking();
+  LED7.blinking();
+  LED8.blinking();
 }
