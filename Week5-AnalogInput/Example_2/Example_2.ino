fb9ef3213e7f6048eefe8f16ebba94c826f013c4
Aiman Ismail <73932427+muhdman@users.noreply.github.com>
2020-11-16 13:49:06 +0800

    Create Example_2.ino

---
+++ Week5-AnalogInput/Example_2/Example_2.ino
@@ -0,0 +1,19 @@
+char *admux = (char*) 0x7C;
+volatile unsigned char *adcsra = (unsigned char*) 0x7A;
+volatile unsigned char *adch = (unsigned char*) 0x79;
+volatile unsigned char *adcl = (unsigned char*) 0x78;
+
+void setup() {
+  // put your setup code here, to run once:
+  *admux = 0b01000000;
+  *adcsra = 0b11000111;
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int lowbyte = (*adcl);
+  int highbyte = (*adch);
+  int value = (highbyte << 8) | lowbyte;
+  Serial.println(value);
+}
